1e755a163f6942a9b1454384d5453c2393e96648
raulcarvalho2 <raulcarvalho2003@gmail.com>
2024-05-12 18:26:32 -0300

    att

---
+++ script.js
@@ -0,0 +1,37 @@
+/*Rolagem para o topo*/
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topButton").style.display = "block";
+    } else {
+        document.getElementById("topButton").style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    var currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (currentPosition > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo(0, currentPosition - currentPosition / 8);
+    }
+}
+/*Fim da rolagem para o topo*/
+
+
+/*Click suave para respectivo assunto:Skill,Projetos,Formacao,Contato*/
+
+document.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+/* Fim do Click suave para respectivo assunto:Skill,Projetos,Formacao*/
